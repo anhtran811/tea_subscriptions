79c30a26895136047af62816f4b6a066b2b17ea9
Anhnhi Tran <anhtran811@gmail.com>
2023-04-18 16:37:14 -0600

    Added dependent destroy to tea model

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,6 +1,6 @@
 class Tea < ApplicationRecord
-  has_many :subscriptions
+  has_many :subscriptions, dependent: :destroy
   has_many :customers, through: :subscriptions, dependent: :destroy
 
-  validates_presence_of :name, :type, :description, :brew_time, :price
+  validates_presence_of :name, :variety, :description, :brew_time, :price
 end
