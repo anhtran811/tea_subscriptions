c69254def6643851adb86bd63fb3a846ff5b23e1
Anhnhi Tran <anhtran811@gmail.com>
2023-04-18 15:36:22 -0600

    Updated relationships following schema changes

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,6 +1,5 @@
 class Tea < ApplicationRecord
-  has_many :tea_subscriptions, dependent: :destroy
-  has_many :subscriptions, through: :tea_subscriptions, dependent: :destroy
+  has_many :subscriptions
   has_many :customers, through: :subscriptions, dependent: :destroy
 
   validates_presence_of :name, :type, :description, :brew_time, :price
