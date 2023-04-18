839989ada7fabb3f1cf96649194275f52b860d82
Anhnhi Tran <anhtran811@gmail.com>
2023-04-18 16:36:14 -0600

    Updated type column name to be variety

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_18_212451) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_18_215233) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,7 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_18_212451) do
 
   create_table "teas", force: :cascade do |t|
     t.string "name"
-    t.string "type"
+    t.string "variety"
     t.string "description"
     t.string "brew_time"
     t.float "price"
